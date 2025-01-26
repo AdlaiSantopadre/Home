c6210e69b171df45f7d97f9c9aa463d69df259cd
Adlai さ ん <126016337+AdlaiSantopadre@users.noreply.github.com>
2025-01-26 17:14:54 +0100

    ulteriore automazione dell`avvio di setup.bat

--- setup_nodes.bat
+++ setup_nodes.bat
@@ -1,4 +1,12 @@
 @echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+REM Imposta le directory di sorgente e di destinazione
+SET SRC_DIR=C:\Users\campus.uniurb.it\Erlang\src
+SET EBIN_DIR=C:\Users\campus.uniurb.it\Erlang\ebin
+REM Esegue PowerShell per compilare tutti i file .erl
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Get-ChildItem -Path '%SRC_DIR%\*.erl' -Recurse | ForEach-Object { erlc -o '%EBIN_DIR%' $_.FullName }"
+ENDLOCAL
+
 REM Imposta la directory di base
 set BASE_DIR=C:\Users\campus.uniurb.it\Erlang
 
@@ -22,8 +30,8 @@ if exist "%BASE_DIR%\%%N@DESKTOPQ2A2FL7_data" (
     mkdir "%BASE_DIR%\%%N@DESKTOPQ2A2FL7_data"
 
     REM Avvia il nodo in una nuova finestra di PowerShell
-     start powershell -NoExit -Command "& erl -sname %%N@DESKTOPQ2A2FL7 -setcookie %COOKIE% -pa %BASE_DIR% -pa %BASE_DIR%/ebin -config %%N"
+     start powershell -NoExit -Command "& erl -sname %%N@DESKTOPQ2A2FL7 -setcookie %COOKIE% -pa %BASE_DIR% -pa %BASE_DIR%\ebin -pa %BASE_DIR%\src -config %%N"
 )
 REM Avvia il nodo di monitor service 
- start powershell -NoExit -Command "erl -sname monitor_service@DESKTOPQ2A2FL7 -setcookie %COOKIE% -pa %BASE_DIR% %BASE_DIR%/ebin -eval "cluster_setup:start_cluster`(`)"
+ start powershell -NoExit -Command "erl -sname monitor_service@DESKTOPQ2A2FL7 -setcookie %COOKIE% -pa %BASE_DIR% %BASE_DIR%\ebin -pa %BASE_DIR%\src -eval "cluster_setup:start_cluster`(`)"
 echo I nodi sono stati avviati.
